3fa52cca46b3f2ab60354a0c75cb4e1d7f4253be
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-03-06 16:35:37 +0200

    add tasts

---
+++ js/modulone.js
@@ -0,0 +1,135 @@
+"use strict";
+
+// // Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
+
+// const apples = 47;
+// const grapes = 135;
+// const total = apples + grapes;
+// console.log(total)
+// const diff = apples - grapes;
+// console.log(diff)
+
+
+//????????????????
+
+
+// // Заміни вираз перевизначення комбінованим оператором +=.
+
+// let students = 100;
+// students += 50;
+// console.log(students);
+
+//??????????????
+
+// // Розбери пріоритет операторів в інструкції привласнення значення змінної result.
+
+// const result = (108 + (223 - 2) * 5);
+// console.log(result);
+
+
+//??????????????
+
+// console.log(('b' + 'a' + + 'a' +'a').toLowerCase());
+
+
+//??????????????
+
+
+// let myVar = 87;
+
+// console.log(myVar++);
+
+//??????????
+
+
+// const remainder = 11 % 3;
+// console.log(remainder);
+
+//?????????
+
+// const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+// console.log(myStr);
+
+//???????????
+
+// const myStr = "FirstLine\n\t\\SecondLine\n\ThirdLine";
+// console.log(myStr);
+
+//??????????
+
+// const myNoun = "dog";
+// const myAdjective = "big";
+// const myVerb = "ran";
+// const myAdverb = "quickly";
+
+// const wordBlanks = "My " + myNoun + " is " + myAdjective + " and really " + myVerb + " also so " + myAdverb;
+// console.log(wordBlanks);
+
+//????????
+
+// const myArray = [["John", 23], ["cat", 2]];
+
+// myArray.push(["dog", 3]);
+// console.log(myArray);
+
+//??????????
+
+// const myArray = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [[10, 11, 12], 13, 14],
+//   ];
+
+//   const myData = myArray[2][1];
+// console.log(myData);
+
+//?????????
+
+// const myArray = [["John", 23], ["cat", 2]];
+
+// myArray.push(["dog", 3]);
+
+// console.log(myArray);
+
+//??????
+
+// const myArray = [["John", 23], ["cat", 2]];
+
+// const removedFromMyArray = myArray.pop();
+
+// console.log(removedFromMyArray);
+
+//??????????
+
+// const myArray = [["John", 23], ["dog", 3]];
+
+// const removedFromMyArray = myArray.shift();
+
+// console.log(removedFromMyArray);
+
+//????????
+
+// const myArray = [["John", 23], ["dog", 3]];
+// myArray.shift();
+
+// myArray.unshift(["Paul", 35]);
+
+// console.log(myArray);
+
+//??????????
+
+// let processed = 2;
+
+// function processArg(num) {
+// return (num + 3) / 5;
+// }
+
+// console.log(processed = processArg(7));
+
+//???????????
+
+
+
+
+
