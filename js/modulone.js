0837e4ea840496e83e753e76b8bbdcd15c0a5dff
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-04-11 13:54:29 +0300

    add diferent examples

--- js/modulone.js
+++ js/modulone.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // // Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
 
 // const apples = 47;
@@ -128,6 +126,175 @@
 // console.log(processed = processArg(7));
 
 //???????????
+
+// function nextInLine(arr, item) {
+//     arr.push(item);
+//     const removed = arr.shift();
+//     return removed;
+// }
+
+// let testArr = [1, 2, 3, 4, 5];
+
+// console.log("Before: " + JSON.stringify(testArr));
+// console.log(nextInLine(testArr, 6));
+// console.log("After: " + JSON.stringify(testArr));
+
+//????????????
+
+// function trueOrFalse(wasThatTrue) {
+//     if (wasThatTrue) {
+//     return "Yes, that was true";
+//     }
+//     return "No, that was false";
+// }
+
+//??????
+
+// function testEqual(val) {
+//     if (val == 12) {
+//     return "Equal";
+//     }
+//     return "Not Equal";
+// }
+
+// testEqual(10);
+
+//???????????
+
+// function testStrict(val) {
+//     if (val === 7) {
+//     return "Equal";
+//     }
+//     return "Not Equal";
+// }
+
+// testStrict(10);
+
+//?????????????
+
+// function compareEquality(a, b) {
+//     if (a === b) {
+//     return "Equal";
+// }
+// return "Not Equal";
+// }
+
+// compareEquality(10, "10");
+
+// ????????????
+
+// function testNotEqual(val) {
+//     if (val != 99) {
+//     return "Not Equal";
+// }
+// return "Equal";
+// }
+
+// testNotEqual(10);
+
+//???????????
+
+// function testStrictNotEqual(val) {
+//     if (val !== 17) {
+//     return "Not Equal";
+// }
+// return "Equal";
+// }
+
+// testStrictNotEqual(10);
+
+//?????????????
+
+// function testGreaterThan(val) {
+//     if (val > 100) {
+//         return "Over 100";
+//     }
+
+//     if (val > 10) {
+//         return "Over 10";
+//     }
+
+//     return "10 or Under";
+// }
+
+// testGreaterThan(10);
+
+//??????????????
+
+// function testGreaterOrEqual(val) {
+//     if (val >= 20) {  // Змініть цей рядок
+//         return "20 or Over";
+//     }
+
+//     if (val >= 10) {  // Змініть цей рядок
+//         return "10 or Over";
+//     }
+
+//     return "Less than 10";
+// }
+
+// testGreaterOrEqual(10);
+
+//??????????????
+
+
+// function testLessThan(val) {
+//     if (val < 25) {
+//         return "Under 25";
+//     }
+
+//     if (val < 55) {
+//         return "Under 55";
+//     }
+
+//     return "55 or Over";
+// }
+
+// testLessThan(10);
+
+//????????????
+
+// function testLessOrEqual(val) {
+//     if (val <= 12) {
+//         return "Smaller Than or Equal to 12";
+//     }
+
+//     if (val <= 24) {
+//         return "Smaller Than or Equal to 24";
+//     }
+
+//     return "More Than 24";
+// }
+
+// testLessOrEqual(10);
+
+//?????????????
+
+// function testLogicalAnd(val) {
+//     if (val >= 25 && val <= 50) {
+//         return "Yes";
+//     }
+//     return "No";
+// }
+
+// console.log(testLogicalAnd(10));
+
+//??????????
+
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = body.firstElementChild;
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const listItems = list.children;
+console.log(listItems);
+
 
 
 
